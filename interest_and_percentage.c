41b85a5c5c51d64b497b7850fae63efd732adf37
riyadMunauwar <contact.riyadmunauwar@gmail.com>
2020-05-04 11:39:04 +0600

    interest funtion bug fix

--- interest_and_percentage.c
+++ interest_and_percentage.c
@@ -1,9 +1,11 @@
- 
 #include <stdio.h>
 
 
 float interest_counter(int total, int part, int percent){
 
+  if(!total) return (float) ( part * 100 ) / percent;
+  if( !part ) return (float) ( percent * total ) / 100;
+  if( !percent ) return (float) ( part *  100 ) / total;
 
  return 0;
 }
@@ -14,10 +16,10 @@ int main(void) {
 
 float res = interest_counter(1000, 0,50);
 
-if( res ) printf(%f, res);
+if( res ) printf("%f", res);
 
 
 
 
   return 0;
-} 
+}
