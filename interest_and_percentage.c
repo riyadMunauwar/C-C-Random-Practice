c3195594d1e1b691f0036fe866aafa9468998f38
riyadMunauwar <contact.riyadmunauwar@gmail.com>
2020-05-04 11:36:12 +0600

    interest and percentage funtion added

---
+++ interest_and_percentage.c
@@ -0,0 +1,23 @@
+ 
+#include <stdio.h>
+
+
+float interest_counter(int total, int part, int percent){
+
+
+ return 0;
+}
+
+
+int main(void) {
+
+
+float res = interest_counter(1000, 0,50);
+
+if( res ) printf(%f, res);
+
+
+
+
+  return 0;
+} 
